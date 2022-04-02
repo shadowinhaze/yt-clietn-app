f4257d178e9ef73adcecaf09c99699db493830cd
@shadowinhaze <shadowinhaze@gmail.com>
2022-04-03 01:30:31 +0200

    feat: implement header component

--- yt-client-app/src/app/components/header/header.component.ts
+++ yt-client-app/src/app/components/header/header.component.ts
@@ -1,8 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { TITLE } from 'src/app/app.component';
+import { SettingsService } from 'src/app/services/settings.sevice';
 
 @Component({
   selector: 'yt-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  public mainTitle = TITLE;
+
+  public searchRequest: string = '';
+
+  public isSettingsActive: boolean = false;
+
+  @Output() setSortType = new EventEmitter();
+
+  @Output() filterValueChange = new EventEmitter();
+
+  @Output() searchValueChange = new EventEmitter();
+
+  constructor(public settingsService: SettingsService) {}
+
+  showSettings() {
+    this.isSettingsActive = !this.isSettingsActive;
+  }
+
+  onFilterValueChange() {
+    this.setSortType.emit();
+    this.filterValueChange.emit();
+  }
+
+  onSortTypeChange() {
+    this.setSortType.emit();
+  }
+
+  onSearchValueSubmit() {
+    this.settingsService.searchValue = 'Angular';
+    this.isSettingsActive = true;
+    this.searchValueChange.emit();
+  }
+}
