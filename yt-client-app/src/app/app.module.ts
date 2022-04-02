db49504adc257a441b8f231ed09251cecb4b092b
@shadowinhaze <shadowinhaze@gmail.com>
2022-04-03 01:40:13 +0200

    feat: add all using declarations, imports and root providers

--- yt-client-app/src/app/app.module.ts
+++ yt-client-app/src/app/app.module.ts
@@ -1,6 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Angular Material
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+// ================
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -12,7 +23,11 @@ import { SearchResultsItemComponent } from './components/search-results-item/sea
 import { SearchResultsItemMoreComponent } from './components/search-results-item-more/search-results-item-more.component';
 import { FilterSortComponent } from './components/filter-sort/filter-sort.component';
 import { LoginComponent } from './components/header/login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReduceNumberPipe } from './pipes/reduce-number.pipe';
+import { CardBorderColorDirective } from './directives/border-color.directive';
+import { DataService } from './services/data.service';
+import { SortDataService } from './services/sort-data-service';
+import { FilterDataService } from './services/filter-data-service';
 
 @NgModule({
   declarations: [
@@ -26,9 +41,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SearchResultsItemMoreComponent,
     FilterSortComponent,
     LoginComponent,
+    ReduceNumberPipe,
+    CardBorderColorDirective,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    FormsModule,
+  ],
+  providers: [DataService, SortDataService, FilterDataService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
