cdf6a0856eac0921193110116be2e8ba22d66f0f
@shadowinhaze <shadowinhaze@gmail.com>
2022-04-03 01:39:10 +0200

    feat: implement main app component

--- yt-client-app/src/app/app.component.ts
+++ yt-client-app/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SearchItem } from './models/search-item.model';
+import { DataService } from './services/data.service';
+import { SettingsService } from './services/settings.sevice';
 
 const TITLE = 'yt-client-app';
 
@@ -8,8 +11,39 @@ const TITLE = 'yt-client-app';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-class AppComponent {
-  title = TITLE;
+class AppComponent implements OnInit {
+  public title = TITLE;
+
+  public results: SearchItem[] = [];
+
+  public showResultsList: boolean = false;
+
+  constructor(
+    private dataService: DataService,
+    private settingsService: SettingsService
+  ) {}
+
+  ngOnInit() {
+    this.results = this.dataService.ytData;
+  }
+
+  filterValueChange() {
+    this.results = this.dataService.filterData(
+      this.settingsService.filterValue
+    );
+  }
+
+  setSortType() {
+    this.results = this.dataService.sortData(
+      this.results,
+      this.settingsService.sortType,
+      this.settingsService.sortDirection
+    );
+  }
+
+  searchValueChange() {
+    this.showResultsList = true;
+  }
 }
 
 export { TITLE, AppComponent };
