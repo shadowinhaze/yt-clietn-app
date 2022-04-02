5635400dcee4756fe81bce65d81dd87e38c72be4
@shadowinhaze <shadowinhaze@gmail.com>
2022-04-03 01:29:06 +0200

    feat: implement filter-sort component

--- yt-client-app/src/app/components/filter-sort/filter-sort.component.ts
+++ yt-client-app/src/app/components/filter-sort/filter-sort.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { SettingsService } from 'src/app/services/settings.sevice';
 
 @Component({
   selector: 'yt-filter-sort',
   templateUrl: './filter-sort.component.html',
   styleUrls: ['./filter-sort.component.scss'],
 })
-export class FilterSortComponent {}
+export class FilterSortComponent {
+  @Output() sortTypeChange = new EventEmitter();
+
+  @Output() filterValueChange = new EventEmitter();
+
+  constructor(public settingsService: SettingsService) {}
+
+  actionSort(type: SortType) {
+    this.settingsService.sortType = type;
+    this.sortTypeChange.emit();
+  }
+
+  actionFilter(model: string) {
+    this.settingsService.filterValue = model;
+    this.settingsService.sortType = '';
+    this.filterValueChange.emit();
+  }
+}
