6749d2de7b38e649204fd1a9c21c1edfc5944a50
@shadowinhaze <shadowinhaze@gmail.com>
2022-04-03 01:29:46 +0200

    feat: implement search-input component

--- yt-client-app/src/app/components/header/search-input/search-input.component.ts
+++ yt-client-app/src/app/components/header/search-input/search-input.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { SettingsService } from 'src/app/services/settings.sevice';
 
 @Component({
   selector: 'yt-search-input',
   templateUrl: './search-input.component.html',
   styleUrls: ['./search-input.component.scss'],
 })
-export class SearchInputComponent {}
+export class SearchInputComponent {
+  @Output() searchValueSubmit = new EventEmitter();
+
+  constructor(public settingsService: SettingsService) {}
+
+  action() {
+    this.searchValueSubmit.emit();
+  }
+}
