d4d24c7fa3525236e640ab0dff338d0e8039025f
@shadowinhaze <shadowinhaze@gmail.com>
2022-04-11 00:10:04 +0200

    feat: implement not-found-page

---
+++ yt-client-app/src/app/core/pages/not-found-page/not-found-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yt-not-found-page',
+  templateUrl: './not-found-page.component.html',
+  styleUrls: ['./not-found-page.component.scss'],
+})
+export class NotFoundPageComponent {}
