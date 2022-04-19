329bafa1044a26f70434dc72e4139d0f8561e22c
@shadowinhaze <shadowinhaze@gmail.com>
2022-04-19 23:50:22 +0200

    refactor: add necessary checking acc. BE problems

--- yt-client-app/src/app/shared/pipes/reduce-number.pipe.ts
+++ yt-client-app/src/app/shared/pipes/reduce-number.pipe.ts
@@ -6,7 +6,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ReduceNumberPipe implements PipeTransform {
   transform(value: string): string {
-    if (value.length >= 7) return `${(Number(value) / 1000000).toFixed(1)} mln`;
+    // Check if number is upsent
+    if (!value) return '0';
+
+    // Check if number is as mln
+    if (value.toString().length >= 7)
+      return `${(Number(value) / 1000000).toFixed(1)} mln`;
+
+    // Check if number is as ths
     return Number(value) > 1000
       ? `${(Number(value) / 1000).toFixed(1)} k`
       : value;
