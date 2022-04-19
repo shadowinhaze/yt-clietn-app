ff25b0848b0c8b4a96f263aa0423e67e13657642
@shadowinhaze <shadowinhaze@gmail.com>
2022-04-19 23:49:06 +0200

    refactor: reorganize constants by groups

---
+++ yt-client-app/src/app/shared/constants/shared-api-constants.ts
@@ -0,0 +1,23 @@
+export enum RequestsShortKeys {
+  video = 'video',
+  search = 'search',
+}
+
+export enum RequestValues {
+  base = 'https://www.googleapis.com/youtube/v3/',
+  video = 'videos',
+  search = 'search',
+}
+
+export enum YouTubeHttpParams {
+  part = 'part',
+  partSnip = 'snippet',
+  partStats = 'statistics',
+  type = 'type',
+  typeVid = 'video',
+  maxResults = 'maxResults',
+  search = 'q',
+  id = 'id',
+}
+
+export const RESULTS_LIST_SIZE = '15';
