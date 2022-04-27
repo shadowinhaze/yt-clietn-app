802cf5eaef5ffa80020953560ff1d72e73777bde
@shadowinhaze <shadowinhaze@gmail.com>
2022-04-28 00:25:05 +0200

    minor: remove mock-data from ngrx store init state

--- yt-client-app/src/app/store/store.model.ts
+++ yt-client-app/src/app/store/store.model.ts
@@ -12,17 +12,7 @@ export interface AppStore {
 }
 
 export const initialState: AppStore = {
-  customItems: [
-    {
-      id: '1123123-123214234-345345',
-      title: 'test',
-      description: 'lorem',
-      imageLink:
-        'https://images.unsplash.com/photo-1650744698456-f80b68db8345?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
-      videoLink: 'https://www.youtube.com/watch?v=xObCVNJRE3s',
-      creationDate: new Date(),
-    },
-  ],
+  customItems: [],
   apiItems: {
     raw: [],
     current: [],
