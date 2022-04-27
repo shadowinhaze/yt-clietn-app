76f8b8be4c09e2fd66662359419ec4f1e0c2b295
@shadowinhaze <shadowinhaze@gmail.com>
2022-04-27 23:27:34 +0200

    feat: connect ngrx and redux browser plugin

--- yt-client-app/src/app/app.module.ts
+++ yt-client-app/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 // ================
 
 import { AppComponent } from './app.component';
 import { interceptorProviders } from './core/interceptors/interceptors';
+import { reducers } from './store/reducers';
+import { environment } from '../environments/environment';
+import { ApiItemsEffect } from './store/effects/api-items.effects';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,6 +24,12 @@ import { interceptorProviders } from './core/interceptors/interceptors';
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([ApiItemsEffect]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   bootstrap: [AppComponent],
   providers: [interceptorProviders],
