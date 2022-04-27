b5e18cf9951904b18acb27d81869ecbf160cbfbd
@shadowinhaze <shadowinhaze@gmail.com>
2022-04-28 00:16:50 +0200

    refactor: change resolver process for working with ngrx

--- yt-client-app/src/app/youtube/resolvers/results-item-more.resolver.ts
+++ yt-client-app/src/app/youtube/resolvers/results-item-more.resolver.ts
@@ -1,19 +1,80 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription, tap } from 'rxjs';
+import { ApiService } from 'src/app/core/services/api.service';
+import { AppStore } from 'src/app/store/store.model';
+import { CustomItem } from '../models/custom-item.model';
 import { SearchItemShort } from '../models/search-item.model';
-import { DataService } from '../../core/services/data.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ResultsItemMoreResolver implements Resolve<SearchItemShort> {
-  constructor(private dataService: DataService, private router: Router) {}
+export class ResultsItemMoreResolver
+  implements
+    Resolve<
+      | CustomItem
+      | SearchItemShort
+      | Observable<SearchItemShort>
+      | Promise<boolean>
+    >,
+    OnDestroy
+{
+  private customItems: CustomItem[] = [];
+
+  private rawApiItems: SearchItemShort[] = [];
+
+  private subscription = new Subscription();
+
+  constructor(
+    private router: Router,
+    private store: Store<AppStore>,
+    private apiService: ApiService
+  ) {
+    this.subscription.add(
+      this.store.select('apiItems').subscribe((apiItems) => {
+        this.rawApiItems = [...apiItems.raw];
+      })
+    );
+
+    this.subscription.add(
+      this.store.select('customItems').subscribe((customItems) => {
+        this.customItems = [...customItems];
+      })
+    );
+  }
 
   resolve(
     route: ActivatedRouteSnapshot
-  ): SearchItemShort | Observable<SearchItemShort> {
-    const item = this.dataService.getItemById(route.params['id']);
-    return item || this.router.navigate(['**']);
+  ):
+    | CustomItem
+    | SearchItemShort
+    | Observable<SearchItemShort>
+    | Promise<boolean> {
+    const { id } = route.params;
+    const apiItem = this.getApiItemById(id);
+    const customItem = this.getCustomItemById(id);
+
+    return customItem || apiItem || this.router.navigate(['**']);
+  }
+
+  getApiItemById(
+    id: string
+  ): SearchItemShort | Observable<SearchItemShort> | undefined {
+    return this.rawApiItems.length > 0
+      ? this.rawApiItems.find((item) => item.id === id)!
+      : this.apiService.getItemById(id).pipe(
+          tap((data) => {
+            if (!data) this.router.navigate(['**']);
+          })
+        );
+  }
+
+  getCustomItemById(id: string): CustomItem | undefined {
+    return this.customItems.find((item) => item.id === id);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
